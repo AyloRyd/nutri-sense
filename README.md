a512fdc392761a434c77f580abaf7267c77fbd2c
AyloRyd <ayloryd@proton.me>
2025-12-07 20:40:36 +0100

    chore: set up a project template

---
+++ README.md
@@ -0,0 +1,7 @@
+# REST API for Code Analysis and Refactoring project in NURE
+
+## Built with
+- PostgreSQL
+- TypeScript
+- NestJS
+- Prisma
---
+++ prisma/migrations/20251207155102_init/migration.sql
@@ -0,0 +1,126 @@
+-- CreateEnum
+CREATE TYPE "Sex" AS ENUM ('male', 'female', 'other');
+
+-- CreateEnum
+CREATE TYPE "PlanGoal" AS ENUM ('maintain', 'gain', 'lose');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "hashed_password" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "avatar_url" TEXT,
+    "sex" "Sex",
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "template_foods" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "calories" DOUBLE PRECISION NOT NULL,
+    "protein" DOUBLE PRECISION NOT NULL,
+    "fats" DOUBLE PRECISION NOT NULL,
+    "carbs" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "template_foods_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "template_meals" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+
+    CONSTRAINT "template_meals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "template_meal_foods" (
+    "id" SERIAL NOT NULL,
+    "template_meal_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "calories" DOUBLE PRECISION NOT NULL,
+    "protein" DOUBLE PRECISION NOT NULL,
+    "fats" DOUBLE PRECISION NOT NULL,
+    "carbs" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "template_meal_foods_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "meals" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "meals_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "meal_foods" (
+    "id" SERIAL NOT NULL,
+    "meal_id" INTEGER NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "name" TEXT NOT NULL,
+    "calories" DOUBLE PRECISION NOT NULL,
+    "protein" DOUBLE PRECISION NOT NULL,
+    "fats" DOUBLE PRECISION NOT NULL,
+    "carbs" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "meal_foods_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "plans" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "start_date" TIMESTAMP(3) NOT NULL,
+    "plan" "PlanGoal" NOT NULL,
+    "day_calories" DOUBLE PRECISION NOT NULL,
+    "day_protein" DOUBLE PRECISION NOT NULL,
+    "day_fats" DOUBLE PRECISION NOT NULL,
+    "day_carbs" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "plans_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_measurements" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "height" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "user_measurements_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "template_foods" ADD CONSTRAINT "template_foods_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "template_meals" ADD CONSTRAINT "template_meals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "template_meal_foods" ADD CONSTRAINT "template_meal_foods_template_meal_id_fkey" FOREIGN KEY ("template_meal_id") REFERENCES "template_meals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "meals" ADD CONSTRAINT "meals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "meal_foods" ADD CONSTRAINT "meal_foods_meal_id_fkey" FOREIGN KEY ("meal_id") REFERENCES "meals"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "plans" ADD CONSTRAINT "plans_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_measurements" ADD CONSTRAINT "user_measurements_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
